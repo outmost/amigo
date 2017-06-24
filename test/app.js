05f4efba3372a74020871a5f17a986baf98d67fd
Tom Sutton <tomsutton1984@users.noreply.github.com>
2017-06-24 11:29:21 +0100

    Testing POST
    Adding test for POST requests
    - error handling in a more testy way

--- test/app.js
+++ test/app.js
@@ -7,7 +7,7 @@ process.env.NODE_ENV = 'test'
 let chai = require('chai')
 let chaiHttp = require('chai-http')
 let server = require('../src/app.js')
-// Standard will complain unless we disable linting for the next line, this is .
+// Standard will complain unless we disable linting for the next line.
 let should = chai.should()  // eslint-disable-line
 
 chai.use(chaiHttp)
@@ -18,12 +18,26 @@ describe('/GET message/:id', () => {
     chai.request(server)
           .get('/messages/12345')
           .end((err, res) => {
-            if (err) {
-              return err
-            }
+            should.not.exist(err)
             res.should.have.status(200)
             res.should.be.a('object')
             res.text.should.be.eql('12345')
+            done()
+          })
+  })
+})
+
+// Test the /POST route
+describe('/POST message/:id', () => {
+  it('it should POST a message', (done) => {
+    chai.request(server)
+          .post('/messages')
+          .send('this is a message')
+          .end((err, res) => {
+            should.not.exist(err)
+            res.should.have.status(200)
+            res.should.be.a('object')
+            res.text.should.be.eql('ok')
             done()
           })
   })
