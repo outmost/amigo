9c729cdc1d0de59de0a54cb3b9135ed57d630569
Tom Sutton <tomsutton1984@users.noreply.github.com>
2017-07-04 17:10:46 +0100

    Bug fix
    Limiter causing tests to fail, quick fix to remove limiting from test suite - better long term solution required.

--- src/app.js
+++ src/app.js
@@ -18,9 +18,6 @@ const limiter = new RateLimit({
   max: 50 // limit each IP to 50 requests per windowMs
 })
 
-//  apply limiter to all requests
-app.use(limiter)
-
 // Routes
 app.use('/', routes)
 
@@ -34,6 +31,8 @@ const port = process.env.PORT || 3000
 
 // Listen, only if app (prevents tests from trying to open a second listen)
 if (!module.parent) {
+  // apply limiter to all requests other than testing
+  app.use(limiter)
   app.listen(port, () => {
     console.log('Server listening on port 3000')
   })
